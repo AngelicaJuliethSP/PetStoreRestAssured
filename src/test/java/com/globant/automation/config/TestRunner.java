90f894a73a15b3e0fcc15a719c3fdf8d0b3f9f30
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-06 20:21:46 -0500

    Create TestRunner class

---
+++ src/test/java/com/globant/automation/config/TestRunner.java
@@ -0,0 +1,37 @@
+package com.globant.automation.config;
+
+import org.testng.annotations.BeforeSuite;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestRunner {
+
+    public static final String PROPERTIES_FILE = "src/test/resources/config.properties";
+    public static final Properties PROPERTIES = new Properties();
+
+    private static String baseUrl;
+
+    @BeforeSuite
+    public void setUpEnvironment() {
+        loadProperties();
+        baseUrl = getConfigVariable("url.base");
+    }
+
+    private void loadProperties() {
+        try (FileInputStream fileInputStream = new FileInputStream(PROPERTIES_FILE)) {
+            PROPERTIES.load(fileInputStream);
+        } catch (IOException e) {
+            System.out.println(String.format("Error loading the properties file: %s", e.getMessage()));
+        }
+    }
+
+    private String getConfigVariable(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+}
