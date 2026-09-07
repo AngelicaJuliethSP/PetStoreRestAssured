505182fbb04fc65070dfe53486ebe10c8289693a
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-07 08:53:06 -0500

    Create RequestBuilder class

---
+++ src/test/java/com/globant/automation/request/RequestBuilder.java
@@ -0,0 +1,42 @@
+package com.globant.automation.request;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpHeaders;
+
+import java.util.Map;
+
+public class RequestBuilder {
+
+    private static RequestSpecification baseSpec(String baseUrl) {
+        return RestAssured.given()
+                .baseUri(baseUrl)
+                .header(HttpHeaders.CONTENT_TYPE, ContentType.JSON.getAcceptHeader())
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter());
+    }
+
+    public static Response get(String baseUrl, String path) {
+        return baseSpec(baseUrl).get(path);
+    }
+
+    public static Response getWithParams(String baseUrl, String path, Map<String, String> queryParams) {
+        return baseSpec(baseUrl)
+                .queryParams(queryParams)
+                .get(path);
+    }
+
+    public static Response post(String baseUrl, String path, Object body) {
+        return baseSpec(baseUrl)
+                .body(body)
+                .post(path);
+    }
+
+    public static Response delete(String baseUrl, String path) {
+        return baseSpec(baseUrl).delete(path);
+    }
+}
